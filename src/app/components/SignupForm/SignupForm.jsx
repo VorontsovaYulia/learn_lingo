a72583b9d1efb841bf00cd05be08cbe1b0a24b97
VorontsovaYulia <vorontsovayulia89@gmail.com>
2025-02-28 23:12:26 +0200

    add_css_ SignupForm

--- src/app/components/SignupForm/SignupForm.jsx
+++ src/app/components/SignupForm/SignupForm.jsx
@@ -1,11 +1,20 @@
 'use client';
 
 import { useForm } from "react-hook-form";
+import { useState } from 'react';
+import Image from 'next/image';
 import { schema } from "./schema";
 import { yupResolver } from '@hookform/resolvers/yup';
 import styles from './SignupForm.module.css'
 
 export const SignupForm = () => {
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const onShow = () => {
+        setShowPassword(!showPassword);
+    };
+    
     const {
         register,
         formState: { errors },
@@ -34,9 +43,18 @@ export const SignupForm = () => {
 
                     <input {...register('email')} placeholder="Email" />
                     <p>{errors.email?.message}</p>
-
-                    <input type="password" {...register('password')} placeholder="Password" />
-                    <p>{errors.password?.message}</p>
+                    <div className={styles.password}>
+                        <input className={styles.password} type={showPassword ? 'text' : 'password'} {...register('password')} placeholder="Password" />
+                        <Image
+                            className={styles.img}
+                            src={showPassword ? '/eye-off.svg' : '/eye.svg'}
+                            alt={showPassword ? 'Open-eye' : 'Close-eye'}
+                            width={18}
+                            height={18}
+                            onClick={onShow}
+                        />
+                        <p>{errors.password?.message}</p>
+                    </div>
                 </div>
 
                 <button className={styles.button} type="submit">Sign Up</button>
