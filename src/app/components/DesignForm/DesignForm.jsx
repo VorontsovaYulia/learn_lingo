28abd6c855dadfe538aa407580f01eb2cfe55e34
VorontsovaYulia <vorontsovayulia89@gmail.com>
2025-03-01 19:25:58 +0200

    fix

--- src/app/components/DesignForm/DesignForm.jsx
+++ src/app/components/DesignForm/DesignForm.jsx
@@ -7,7 +7,7 @@ export const DesignForm = ({type}) => {
     return (
         <div className={styles.wrapper}>
             <h3>
-                {type === 'sign-up' ? 'WELCOME' : 'WELCOME BACK!'}
+                {type === 'sign-up' ? 'WELCOME!' : 'WELCOME BACK!'}
             </h3>
             <p>
                 {type === 'sign-up' ? 'Fill out all the information please' : 'To stay connected with us please login with your personal info'}
