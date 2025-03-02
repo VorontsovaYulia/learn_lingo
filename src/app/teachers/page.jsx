03b66997c6fdb1ee460a3a0ff499f8a3565cc49b
VorontsovaYulia <vorontsovayulia89@gmail.com>
2025-03-02 14:56:52 +0200

    added_page_ teachers

--- src/app/teachers/page.jsx
+++ src/app/teachers/page.jsx
@@ -1,44 +1,74 @@
+'use client';
+
+import { shallow } from 'zustand/shallow';
 import Image from "next/image";
 import styles from './teachers.module.css'
+import { useTeachers } from "../store/store";
+import { useEffect } from 'react';
 
 export default function Teachers() {
+  
+   const fetchAllTeachers = useTeachers(state =>state.getAllTeachers, shallow)
+   const allTeachers = useTeachers(state =>state.teachers, shallow)
+
+   useEffect(() => {
+      fetchAllTeachers();
+   }, []);
+   
+   console.log(allTeachers)
+
    return (
       <div className={styles.layout}>
-         <div className={styles.wrapper}>
-            <div>
-               <Image src="/avatar.png" alt="avatar" width={120} height={120} />
-            </div>
-
-            <div className={styles.container}>
-
-               <div className={styles.topbox}>
-                  <p className={styles.text}>Languages</p>
-                  <div className={styles.box}>
-                     <Image src="/book-open.svg" alt="book-open" width={16} height={16} />
-                     <p className={styles.line}>Lessons online</p>
-                  </div>
-            
-                  <p className={styles.line}>Lessons done: 1098</p>
+         {allTeachers.map((el, index) => {
+            return (
                
-                  <div className={styles.box}>
-                     <Image src="/star.svg" alt="star" width={16} height={16} />
-                     <p className={styles.line}>Rating: 4.8</p>
+               <div key={index} className={styles.wrapper}>
+                  <div className={styles['avatar-box']}>
+                     <Image className={styles.avatar} src={el.avatar_url} alt="avatar" width={96} height={96} />
+                     <Image className={styles.online} src='/online.svg' alt="online" width={12} height={12} />
                   </div>
-                  <p>Price / 1 hour: <span>30$</span></p>
-                  <Image src="/empty-heart.svg" alt="empty-heart" width={26} height={26} className={styles.img} />
-               </div>
-               <div className={styles.bottombox}>
-                  <h3>Jane Smith</h3>
-               </div>
 
-               <div className={styles.info}>
-                  <p><span>Speaks: </span>German, French</p>
-                  <p><span>Lesson Info: </span>Lessons are structured to cover grammar, vocabulary, and practical usage of the language.</p>
-                  <p><span>Conditions: </span>Welcomes both adult learners and teenagers (13 years and above).Provides personalized study plans</p>
+                  <div className={styles.container}>
+
+                     <div className={styles.topbox}>
+                        <p className={styles.text}>Languages</p>
+                        <div className={styles.box}>
+                           <Image src="/book-open.svg" alt="book-open" width={16} height={16} />
+                           <p className={styles.line}>Lessons online</p>
+                        </div>
+            
+                        <p className={styles.line}>Lessons done: {el.lessons_done}</p>
+               
+                        <div className={styles.box}>
+                           <Image src="/star.svg" alt="star" width={16} height={16} />
+                           <p className={styles.line}>Rating: {el.rating}</p>
+                        </div>
+                        <p>Price / 1 hour: <span>{el.price_per_hour}$</span></p>
+                        <Image src="/empty-heart.svg" alt="empty-heart" width={26} height={26} className={styles.img} />
+                     </div>
+                     <div className={styles.bottombox}>
+                        <h3>{`${el.name} ${el.surname}`}</h3>
+                     </div>
+
+                     <div className={styles.info}>
+                        
+                        <p> <span>Speaks: </span> {el.languages.slice(0, -1).join(', ')}
+                           {el.languages.length > 1 ? ', ' : ''}
+                           {el.languages[el.languages.length - 1]}
+                        </p>
+
+                        <p><span>Lesson Info: </span>{el.lesson_info}</p>
+                        <p> <span>Conditions: </span>{el.conditions.join(' ')}
+                        </p>
+
+                     </div>
+                     <button className={styles.btn} type="button">Read more</button>
+                  </div>
                </div>
-               <button className={styles.btn} type="button">Read more</button>
-            </div>
-         </div>
+               
+            )
+         })}
+         
       </div>
    );
 }
