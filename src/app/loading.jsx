68ff19c17f697a4020e6996865070270d2a85832
VorontsovaYulia <vorontsovayulia89@gmail.com>
2025-03-05 19:26:29 +0200

    added_loader

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,3 +1,16 @@
+import BeatLoader from "react-spinners/BeatLoader";
+
+const override = {
+    display: "block",
+    margin: "0 auto",
+};
+
 export default function LoadingHome() {
-    return <h1>Loading...</h1>
-}
+    return <BeatLoader
+            color='#9FBAAE'
+            size={15}
+            cssOverride={override}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+    />
+};
