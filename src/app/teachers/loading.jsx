2fb2b299644d7eb2a3fd7678a6384c814ad317e7
VorontsovaYulia <vorontsovayulia89@gmail.com>
2025-03-06 22:30:31 +0200

    fixed_loader

--- src/app/teachers/loading.jsx
+++ src/app/teachers/loading.jsx
@@ -1,16 +1,20 @@
 import BeatLoader from "react-spinners/BeatLoader";
 
-const override = {
-    display: "block",
-    margin: "0 auto",
-};
-
-export default function LoadingTeachers() {
-    return <BeatLoader
-            color='#9FBAAE'
-            size={15}
-            cssOverride={override}
-            aria-label="Loading Spinner"
-            data-testid="loader"
-    />
-};
+export default function LoadingHome() {
+    return (
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            width: "100vw",
+        }}>
+            <BeatLoader
+                color="#9FBAAE"
+                size={15}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />
+        </div>
+    );
+}
