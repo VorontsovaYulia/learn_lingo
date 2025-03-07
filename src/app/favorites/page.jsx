d2f68df4a97e71ae6cfb6c283ac899283aa4c7da
VorontsovaYulia <vorontsovayulia89@gmail.com>
2025-03-07 18:07:52 +0200

    fix_img

--- src/app/favorites/page.jsx
+++ src/app/favorites/page.jsx
@@ -4,7 +4,7 @@ import styles from './favorites.module.css'
 export default function Favorites() {
    return (
       <div className={styles.wrapper}>
-         <Image src='/favorites-icon.svg' alt="empty" width={500} height={500} />
+         <Image src='/favorites-icon.svg' alt="empty" width={500} height={500} priority/>
       </div>
    );
 }
