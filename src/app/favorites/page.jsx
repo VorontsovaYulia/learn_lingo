0a3f130c1e6724e0372ff3c43e7f081395174fac
VorontsovaYulia <vorontsovayulia89@gmail.com>
2025-03-08 13:49:45 +0200

    fixed_favorite

--- src/app/favorites/page.jsx
+++ src/app/favorites/page.jsx
@@ -12,6 +12,7 @@ export default function Favorites() {
 
    return (
       <PrivateRoute>
+         <div className={styles.container}>
          {userFavorites.length > 0 ? (
             <ul className={styles.layout}>
                {userFavorites.map(el => (
@@ -19,12 +20,13 @@ export default function Favorites() {
                      <TeacherCard teacher={el} />
                   </li>
                ))}
-            </ul>
+               </ul>
          ) : (
             <div className={styles.box}>
                <Image className={styles.img} src='/favorites-icon.svg' alt="empty" width={500} height={500} priority />
             </div>
-         )}
+            )}
+            </div>
       </PrivateRoute>
    );
 }
